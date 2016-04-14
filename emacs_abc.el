c3eff79274d437f8cf6a5f90f2d272f7eb7095e2
Maxim Neumann <maximn@amazon.com>
2016-04-13 18:03:34 -0700

    Adding flycheck pylint keybindings

--- emacs_abc.el
+++ emacs_abc.el
@@ -271,39 +271,17 @@
 (add-hook 'LaTeX-mode-hook 'flyspell-mode)  ; automatic start of flyspell
 
 
-;;;; Custom functions
 
-;;; Count words in a region
-(defun count-words-region (beginning end)
-  "Print number of words in the region."
-  (interactive "r")
-  (message "Counting words in region ... ")
-;;; 1. Set up appropriate conditions.
-  (save-excursion
-    (let ((count 0))
-      (goto-char beginning)
-;;; 2. Run the while loop.
-      (while (and (< (point) end)
-                  (re-search-forward "\\w+\\W*" end t))
-        (setq count (1+ count)))
-;;; 3. Send a message to the user.
-      (cond ((zerop count)
-             (message
-              "The region does NOT have any words."))
-            ((= 1 count)
-             (message
-              "The region has 1 word."))
-            (t
-             (message
-              "The region has %d words." count))))))
+;;;; Flycheck + pylint
 
-
-;;;; Flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 ;(global-flycheck-mode)
 (setq flycheck-checker-error-threshold 1000) ;; or switch back to default 400
 (setq flycheck-pylintrc ".pylintrc")
 (setq flycheck-keymap-prefix (kbd "C-c *")) ;; changing the flycheck prefix to C-c *
+(global-set-key (kbd "C-c n n") 'flycheck-next-error)
+(global-set-key (kbd "C-c n p") 'flycheck-prev-error)
+
 
 
 ;;;; Projectile
@@ -346,6 +324,34 @@
 
 ;; markdown-mode
 (setq auto-mode-alist (cons '("\\.md$" . markdown-mode) auto-mode-alist))
+
+
+;;;; Custom functions
+
+;;; Count words in a region
+(defun count-words-region (beginning end)
+  "Print number of words in the region."
+  (interactive "r")
+  (message "Counting words in region ... ")
+;;; 1. Set up appropriate conditions.
+  (save-excursion
+    (let ((count 0))
+      (goto-char beginning)
+;;; 2. Run the while loop.
+      (while (and (< (point) end)
+                  (re-search-forward "\\w+\\W*" end t))
+        (setq count (1+ count)))
+;;; 3. Send a message to the user.
+      (cond ((zerop count)
+             (message
+              "The region does NOT have any words."))
+            ((= 1 count)
+             (message
+              "The region has 1 word."))
+            (t
+             (message
+              "The region has %d words." count))))))
+
 
 
 ;;;; Testing modes/packages
